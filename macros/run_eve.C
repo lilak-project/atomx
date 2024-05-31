f05f79880ce76c2013d9011516541dc97f21a967
Jung Woo Lee <phyjics@gmail.com>
2024-06-01 00:56:14 +0900

    macro parameter update

--- macros/run_eve.C
+++ macros/run_eve.C
@@ -1,22 +1,17 @@
-void next(int eventID=-1)
-{
-    if (eventID<0)
-        LKRun::GetRun() -> ExecuteNextEvent();
-    else
-        LKRun::GetRun() -> ExecuteEvent(eventID);
+void next(int eventID=-1) {
+    if (eventID<0) LKRun::GetRun() -> ExecuteNextEvent();
+    else LKRun::GetRun() -> ExecuteEvent(eventID);
 }
 
 void run_eve()
 {
     auto run = new LKRun();
-    run -> SetTag("eve");
-    run -> AddPar("config_eve.mac");
-    run -> AddInputFile("/home/ejungwoo/data/atomx/atomx_0058.D0.conv.root");
-    //run -> AddFriend("/home/ejungwoo/lilak/atomx/macros/data/atomx_0058.D0.psa.root");
     run -> AddDetector(new AToMX());
-    //run -> Add(new LKGETChannelViewer);
+    run -> AddPar("config_eve.mac");
     run -> Add(new LKPulseShapeAnalysisTask);
+    run -> Add(new LKHTTrackingTask);
     run -> Add(new LKEveTask);
     run -> Init();
-    next(109);
+
+    next(1);
 }
--- macros/run_viewer.C
+++ macros/run_viewer.C
@@ -2,7 +2,8 @@ void run_viewer()
 {
     auto run = new LKRun();
     run -> AddPar("config_viewer.mac");
-    run -> AddInputFile("/home/ejungwoo/data/atomx/atomx_0058.D0.conv.root");
+    //run -> AddInputFile("/home/ejungwoo/data/atomx/atomx_0058.d0.conv.root");
+    run -> AddInputFile("/home/ejungwoo/data/atomx/atomx_0141.conv.root");
     run -> Add(new LKGETChannelViewer);
     run -> Init();
     run -> ExecuteNextEvent();
